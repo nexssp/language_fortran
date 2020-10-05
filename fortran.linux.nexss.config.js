c4efdf3df9844d889119a1056e9be70f63d6c1d1
Marcin Polak <mapoart@gmail.com>
2020-10-05 12:48:55 +0200

    Other distros

--- fortran.linux.nexss.config.js
+++ fortran.linux.nexss.config.js
@@ -1,13 +1,30 @@
 let languageConfig = Object.assign({}, require("./fortran.win32.nexss.config"));
+
+const os = require(`${process.env.NEXSS_SRC_PATH}/node_modules/@nexssp/os/`);
+const sudo = os.sudo();
+
+const distName = os.name();
+
 languageConfig.compilers = {
   gfortran: {
-    install: "apt install gfortran",
+    install: `${sudo}apt install -y gfortran`,
     command: "gfortran",
     args: "<file> -o <file>.exe && ./<file>.exe",
     help: ``,
   },
 };
-languageConfig.errors = require("./nexss.fortran.errors");
-languageConfig.languagePackageManagers = {};
+
+switch (distName) {
+  // case os.distros.ALPINE:
+  //   languageConfig.compilers.gfortran.install = os.replacePMByDistro(
+  //     "apk add gfortran"
+  //   );
+  //   break;
+  default:
+    languageConfig.compilers.gfortran.install = os.replacePMByDistro(
+      languageConfig.compilers.gfortran.install
+    );
+    break;
+}
 
 module.exports = languageConfig;
